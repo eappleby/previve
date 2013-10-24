96cacc5e343bca4cb86c0a72fb673aed651997e6
Evan Appleby <evan@pubexchange.com>
2013-10-23 23:59:56 -0400

    first

--- application/views/home.php
+++ application/views/home.php
@@ -77,6 +77,6 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="<?php echo site_url("assets/js/jquery.js"); ?>"></script>
-    <script src="<?php echo site_url("js/bootstrap.min.js"); ?>"></script>
+    <script src="<?php echo site_url("assets/js/bootstrap.min.js"); ?>"></script>
   </body>
 </html>
