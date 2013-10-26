4510db8815a015f4adeec87fd8cd80797141ff00
Evan Appleby <evan@pubexchange.com>
2013-10-25 21:21:21 -0400

    formatting

--- index.php
+++ index.php
@@ -1,166 +1,152 @@
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Previve</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <!-- Le styles -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-    <style type="text/css">
-      body {
-        padding-top: 20px;
-        padding-bottom: 40px;
-
-		/* IE10 Consumer Preview */ 
-		background-image: -ms-linear-gradient(bottom, #EDFFFF 0%, #FFFFFF 50%);
-
-		/* Mozilla Firefox */ 
-		background-image: -moz-linear-gradient(bottom, #EDFFFF 0%, #FFFFFF 50%);
-
-		/* Opera */ 
-		background-image: -o-linear-gradient(bottom, #EDFFFF 0%, #FFFFFF 50%);
-
-		/* Webkit (Safari/Chrome 10) */ 
-		background-image: -webkit-gradient(linear, left bottom, left top, color-stop(0, #EDFFFF), color-stop(.5, #FFFFFF));
-
-		/* Webkit (Chrome 11+) */ 
-		background-image: -webkit-linear-gradient(bottom, #EDFFFF 0%, #FFFFFF 50%);
-
-		/* W3C Markup, IE10 Release Preview */ 
-		background-image: linear-gradient(to top, #EDFFFF 0%, #FFFFFF 50%);      
-	  }
-
-.featurette {
-padding-top: 120px;
-overflow:hidden;
-}
-
-.featurette-image {
-padding-right:50px;	
-}
-
-.featurette-image img {
-height: auto;
-max-width: 100%;
-vertical-align: middle;
-border: 0;	
-}
-
-@media (max-width: 980px) {
-.featurette-image.pull-left, .featurette-image.pull-right {
-display: block;
-float: none;
-max-width: 50%;
-margin: 0 auto 20px;
-}
-.featurette {
-height: auto;
-padding: 0;
-}
-}
-@media (max-width: 767px) {
-.featurette-heading {
-font-size: 30px;
-}
-}
-.featurette-image.pull-left {
-margin-right: 40px;
-}
-
-.featurette-image {
-margin-top: -120px;
-}
-
-.featurette-heading {
-font-size: 50px;
-font-weight: 300;
-line-height: 1;
-letter-spacing: -1px;
-}
-
-	  .wrapper {
-	  	background:#ffffff;
-	  }
-
-      .masthead {
-      	text-align:center;
-      	padding-bottom:50px;
-      }
-
-      /* Supporting marketing content */
-      .marketing {
-        margin: 60px 0;
-      }
-      .marketing p + h4 {
-        margin-top: 28px;
-      }
-      .navbar-hp {
-      	text-align:center;
-      }
-    </style>
-    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
+   <head>
+      <meta charset="utf-8">
+      <title>Previve</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- Le styles -->
+      <link href="assets/css/bootstrap.css" rel="stylesheet">
+      <style type="text/css">
+         body {
+         padding-top: 20px;
+         padding-bottom: 40px;
+         /* IE10 Consumer Preview */ 
+         background-image: -ms-linear-gradient(bottom, #EDFFFF 0%, #FFFFFF 50%);
+         /* Mozilla Firefox */ 
+         background-image: -moz-linear-gradient(bottom, #EDFFFF 0%, #FFFFFF 50%);
+         /* Opera */ 
+         background-image: -o-linear-gradient(bottom, #EDFFFF 0%, #FFFFFF 50%);
+         /* Webkit (Safari/Chrome 10) */ 
+         background-image: -webkit-gradient(linear, left bottom, left top, color-stop(0, #EDFFFF), color-stop(.5, #FFFFFF));
+         /* Webkit (Chrome 11+) */ 
+         background-image: -webkit-linear-gradient(bottom, #EDFFFF 0%, #FFFFFF 50%);
+         /* W3C Markup, IE10 Release Preview */ 
+         background-image: linear-gradient(to top, #EDFFFF 0%, #FFFFFF 50%);      
+         }
+         .featurette {
+         padding-top: 120px;
+         overflow:hidden;
+         padding-bottom:80px;
+         }
+         .featurette-image {
+         padding-right:50px;	
+         }
+         .featurette-image img {
+         height: auto;
+         max-width: 100%;
+         vertical-align: middle;
+         border: 0;	
+         }
+         @media (max-width: 980px) {
+         .featurette-image.pull-left, .featurette-image.pull-right {
+         display: block;
+         float: none;
+         max-width: 50%;
+         margin: 0 auto 20px;
+         }
+         .featurette {
+         height: auto;
+         padding: 0;
+         }
+         }
+         @media (max-width: 767px) {
+         .featurette-heading {
+         font-size: 30px;
+         }
+         }
+         .featurette-image.pull-left {
+         margin-right: 40px;
+         }
+         .featurette-image {
+         margin-top: -120px;
+         }
+         .featurette-heading {
+         font-size: 50px;
+         font-weight: 300;
+         line-height: 1;
+         letter-spacing: -1px;
+         }
+         .wrapper {
+         background:#ffffff;
+         }
+         .wrapper-navbar-hp {
+         margin-bottom:40px;
+         }
+         .wrapper-disclaimer {
+         padding: 20px 0;
+         }
+         .masthead {
+         text-align:center;
+         padding-bottom:50px;
+         }
+         /* Supporting marketing content */
+         .marketing {
+         margin: 60px 0;
+         }
+         .marketing p + h4 {
+         margin-top: 28px;
+         }
+         .navbar-hp {
+         text-align:center;
+         }
+      </style>
+      <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+      <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+      <!--[if lt IE 9]>
       <script src="assets/js/html5shiv.js"></script>
-    <![endif]-->
-
-  </head>
-
-  <body>
-
-    <div class="container">
-      <div class="masthead">
-        <h3 class="logo">Previve</h3>
-        <h4 class="tagline">prevents hangover symptoms, scientifically.</h4>
+      <![endif]-->
+   </head>
+   <body>
+      <div class="container">
+         <div class="masthead">
+            <h3 class="logo">Previve</h3>
+            <h4 class="tagline">prevents hangover symptoms, scientifically.</h4>
+         </div>
+         <div class="featurette">
+            <img class="featurette-image pull-left" src="assets/img/previve-packaging-1-box.jpg">
+            <p><strong>ENERGIZES</strong> your night</p>
+            <p><strong>PREVENTS</strong> hangover symptons</p>
+            <p><strong>PROMOTES</strong> long-term wellness</p>
+         </div>
       </div>
-
-	  <div class="featurette">
-		<img class="featurette-image pull-left" src="assets/img/previve-packaging-1-box.jpg">
-		<p><strong>ENERGIZES</strong> your night</p>
-		<p><strong>PREVENTS</strong> hangover symptons</p>
-		<p><strong>PROMOTES</strong> long-term wellness</p>
-	  </div>
-
-  </div>
-
-  <div class="wrapper">
-
-    <div class="container-narrow">
-
-<div class="row nav-pills">
-  <div class="navbar-hp col-md-2"><a href="#">Shop Online</a></div>
-  <div class="navbar-hp col-md-2"><a href="#">The Previve Story</a></div>
-  <div class="navbar-hp col-md-2"><a href="#">How It Works</a></div>
-  <div class="navbar-hp col-md-2"><a href="#">Why Four Pills?</a></div>
-  <div class="navbar-hp col-md-2"><a href="#">FAQ</a></div>
-  <div class="navbar-hp col-md-2"><a href="#">Press</a></div>
-</div>
-
-    </div> <!-- /container -->
-
-  </div>
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="assets/js/jquery.js"></script>
-    <script src="assets/js/bootstrap-transition.js"></script>
-    <script src="assets/js/bootstrap-alert.js"></script>
-    <script src="assets/js/bootstrap-modal.js"></script>
-    <script src="assets/js/bootstrap-dropdown.js"></script>
-    <script src="assets/js/bootstrap-scrollspy.js"></script>
-    <script src="assets/js/bootstrap-tab.js"></script>
-    <script src="assets/js/bootstrap-tooltip.js"></script>
-    <script src="assets/js/bootstrap-popover.js"></script>
-    <script src="assets/js/bootstrap-button.js"></script>
-    <script src="assets/js/bootstrap-collapse.js"></script>
-    <script src="assets/js/bootstrap-carousel.js"></script>
-    <script src="assets/js/bootstrap-typeahead.js"></script>
-
-  </body>
+      <div class="wrapper wrapper-navbar-hp">
+         <div class="container">
+            <div class="row nav-pills">
+               <div class="navbar-hp col-md-2"><a href="#">Shop Online</a></div>
+               <div class="navbar-hp col-md-2"><a href="#">The Previve Story</a></div>
+               <div class="navbar-hp col-md-2"><a href="#">How It Works</a></div>
+               <div class="navbar-hp col-md-2"><a href="#">Why Four Pills?</a></div>
+               <div class="navbar-hp col-md-2"><a href="#">FAQ</a></div>
+               <div class="navbar-hp col-md-2"><a href="#">Press</a></div>
+            </div>
+         </div>
+         <!-- /container -->
+      </div>
+      <div class="wrapper wrapper-disclaimer">
+         <div class="container">
+            <p>
+               * Remember, this product does not prevent intoxication, alcohol poisoning, or alcohol abuse. Please drink responsibly. Never drink and drive! Do not consume alcohol if you are not of the legal age to do so, while pregnant or 
+               nursing. These statements have not been evaluated by the Food and Drug Administration and Previve is not intended to diagnose, mitigate, treat, cure, or prevent any disease. Practice safe drinking!
+            </p>
+         </div>
+      </div>
+      <!-- Le javascript
+         ================================================== -->
+      <!-- Placed at the end of the document so the pages load faster -->
+      <script src="assets/js/jquery.js"></script>
+      <script src="assets/js/bootstrap-transition.js"></script>
+      <script src="assets/js/bootstrap-alert.js"></script>
+      <script src="assets/js/bootstrap-modal.js"></script>
+      <script src="assets/js/bootstrap-dropdown.js"></script>
+      <script src="assets/js/bootstrap-scrollspy.js"></script>
+      <script src="assets/js/bootstrap-tab.js"></script>
+      <script src="assets/js/bootstrap-tooltip.js"></script>
+      <script src="assets/js/bootstrap-popover.js"></script>
+      <script src="assets/js/bootstrap-button.js"></script>
+      <script src="assets/js/bootstrap-collapse.js"></script>
+      <script src="assets/js/bootstrap-carousel.js"></script>
+      <script src="assets/js/bootstrap-typeahead.js"></script>
+   </body>
 </html>
