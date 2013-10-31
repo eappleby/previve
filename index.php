175ef54fdd65ece6cedad4ea37c5a5622ffa185f
Evan Appleby <evan@pubexchange.com>
2013-10-31 00:04:27 -0400

    initial complete checkin

--- index.php
+++ index.php
@@ -1,231 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-      <meta charset="utf-8">
-      <title>PREVIVE - prevents hangover symptoms, scientifically.</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="description" content="">
-      <meta name="author" content="">
-      <!-- Le styles -->
-      <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-      <style type="text/css">
-		@font-face
-		{
-		font-family: gothamBlack;
-		src: url(assets/fonts/Gotham-Black.otf);
-		}
-		@font-face
-		{
-		font-family: gothamBlackItalic;
-		src: url(assets/fonts/Gotham-Blackitalic.otf);
-		}
-		@font-face
-		{
-		font-family: gothamBold;
-		src: url(assets/fonts/Gotham-Bold.otf);
-		}
-		@font-face
-		{
-		font-family: gothamBoldItalic;
-		src: url(assets/fonts/Gotham-BoldItalic.otf);
-		}
-		@font-face
-		{
-		font-family: gothamBook;
-		src: url(assets/fonts/Gotham-Book.otf);
-		}
-		@font-face
-		{
-		font-family: gothamBookItalic;
-		src: url(assets/fonts/Gotham-BookItalic.otf);
-		}
-		@font-face
-		{
-		font-family: gothamExtraLight;
-		src: url(assets/fonts/Gotham-extralight.otf);
-		}
-		@font-face
-		{
-		font-family: gothamExtraLightItalic;
-		src: url(assets/fonts/Gotham-ExtraLightItalic.otf);
-		}
-		@font-face
-		{
-		font-family: gothamLight;
-		src: url(assets/fonts/Gotham-Light.otf);
-		}
-		@font-face
-		{
-		font-family: gothamLightItalic;
-		src: url(assets/fonts/Gotham-LightItalic.otf);
-		}
-		@font-face
-		{
-		font-family: gothamMedium;
-		src: url(assets/fonts/Gotham-Medium.otf);
-		}
-		@font-face
-		{
-		font-family: gothamMediumItalic;
-		src: url(assets/fonts/Gotham-MediumItalic.otf);
-		}
-         body {
-        font-family:gothamLight;
-        padding: 20px 0 0;
-		/* IE10 Consumer Preview */ 
-		background-image: -ms-linear-gradient(bottom, #66CCCC 0%, #FFFFFF 30%);
-		/* Mozilla Firefox */ 
-		background-image: -moz-linear-gradient(bottom, #66CCCC 0%, #FFFFFF 30%);
-		/* Opera */ 
-		background-image: -o-linear-gradient(bottom, #66CCCC 0%, #FFFFFF 30%);
-		/* Webkit (Safari/Chrome 10) */ 
-		background-image: -webkit-gradient(linear, left bottom, left top, color-stop(0, #66CCCC), color-stop(.3, #FFFFFF));
-		/* Webkit (Chrome 11+) */ 
-		background-image: -webkit-linear-gradient(bottom, #66CCCC 0%, #FFFFFF 30%);
-		/* W3C Markup, IE10 Release Preview */ 
-		background-image: linear-gradient(to top, #66CCCC 0%, #FFFFFF 30%);      
-		}
- 		h3.logo{
- 		text-transform: uppercase;
- 		font-family:gothamLight;
- 		font-size:32px;
- 		color: #000080;
- 		}
- 		h3.logo span{
- 		color: #66CCCC;
- 		}
- 		h4.tagline{
- 		font-family:times;
- 		font-style:italic;
- 		font-size:18px;
- 		color: #000080;
- 		}
-         .feature {
-         overflow:hidden;
-         padding-bottom:80px;
-         padding-left: 115px;
-         }
-         .feature-messaging {
-         padding-top:120px;
-         text-align:left;
- 		color: #66CCCC;
- 		font-family:times;
- 		font-size:20px;
-         }
-         .feature-messaging span {
- 		color: #000080;
- 		text-transform: uppercase;
- 		font-family:gothamLight;
- 		font-size: 22px;
-		font-weight: bold;
-         }
-         .feature-image {
-         text-align:right;
-         padding-right: 80px;
-     	 }
-         .feature-image img {
-         height: auto;
-         max-width: 100%;
-         vertical-align: middle;
-         border: 0;	
-         }
-        .wrapper {
-         background:#ffffff;
-         }
-         .wrapper-navbar-hp {
-         margin-bottom:40px;
-         }
-         .wrapper-disclaimer {
-         padding: 20px 0;
-         }
-         .disclaimer {
-         color: #000080;
-         font-size:10px;
-         }
-         .masthead {
-         text-align:center;
-         padding-bottom:50px;
-         }
-         .navbar-hp {
-         text-align:center;
-         text-transform:uppercase;
-         color: #000080;
-         font-size:14px;
-         font-weight:bold;
-         padding: 8px 0;
-         }
-		.inactive-link, .inactive-link:hover {
-		color: #c1c1c1;
-		text-decoration: none;
-		}
-         @media (max-width: 991px) {
-         .feature {
-         padding-left: 40px;
-		 }
-         .feature-messaging {
-         padding-top: 40px;
-		 }
-		 }
-
-         @media (max-width: 767px) {
-         .feature {
-         padding-left: 0px;
-         padding-bottom: 40px;
-		 }
-         .feature-messaging {
-         display:none;
-         }
-         .feature-image {
-         max-width:90%;
-         padding-right: 0;
-         text-align:center;
-         }
-         }
-      </style>
-      <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
-      <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-      <!--[if lt IE 9]>
-      <script src="assets/js/html5shiv.js"></script>
-      <![endif]-->
-   </head>
-   <body>
-      <div class="container">
-         <div class="masthead">
-            <h3 class="logo"><span>pre</span>vive</h3>
-            <h4 class="tagline">prevents hangover symptoms, scientifically.</h4>
-         </div>
-         <div class="row feature">
-         	<div class="col-sm-6 feature-image">
-	            <img src="assets/img/previve-packaging-1-box.png">
-	        </div>
-            <div class="col-sm-6 feature-messaging">
-	            <p><span>ENERGIZES</span> your night</p>
-	            <p><span>PREVENTS</span> hangover symptons</p>
-	            <p><span>PROMOTES</span> long-term wellness</p>
-	        </div>
-         </div>
-      </div>
-      <div class="wrapper wrapper-navbar-hp">
-         <div class="container">
-            <div class="row">
-               <div class="navbar-hp col-sm-2"><a href="#">Shop Online</a></div>
-               <div class="navbar-hp col-sm-2"><a href="previve-story.html">The Previve Story</a></div>
-               <div class="navbar-hp col-sm-2"><a href="how-it-works.html">How It Works</a></div>
-               <div class="navbar-hp col-sm-2"><a href="why-four-pills.html">Why Four Pills?</a></div>
-               <div class="navbar-hp col-sm-2"><a href="faq.html">FAQ</a></div>
-               <div class="navbar-hp col-sm-2"><a class="inactive-link" href="#">Press</a></div>
-            </div>
-         </div>
-      </div>
-      <div class="wrapper wrapper-disclaimer">
-         <div class="container">
-            <p class="disclaimer">
-               Remember, this product does not prevent intoxication, alcohol poisoning, or alcohol abuse. Please drink responsibly. Never drink and drive! Do not consume alcohol if you are not of the legal age to do so, while pregnant or 
-               nursing. These statements have not been evaluated by the Food and Drug Administration and Previve is not intended to diagnose, mitigate, treat, cure, or prevent any disease. Practice safe drinking!
-            </p>
-         </div>
-      </div>
-      <script src="assets/js/jquery.js"></script>
-      <script src="assets/js/bootstrap.min.js"></script>
-   </body>
+	<head>
+	<meta charset="utf-8">
+		<title>PREVIVE - prevents hangover symptoms, scientifically.</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="PREVIVE prevents hangover symptoms, scientifically.">
+		<meta name="author" content="Ian Weber">
+		<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+		<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+		<link href="assets/css/previve.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+		<script src="assets/js/html5shiv.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="container">
+			<div class="masthead">
+				<h3 class="logo"><span class="dark">pre</span>vive</h3>
+				<h4 class="tagline">prevents hangover symptoms, scientifically.</h4>
+			</div>
+			<div class="row feature">
+				<div class="col-sm-6 feature-image">
+					<img src="assets/img/previve-packaging-1-box.png">
+				</div>
+				<div class="col-sm-6 feature-messaging">
+					<p><span class="large">ENERGIZES</span> <span class="light">your night</span></p>
+					<p><span class="large">PREVENTS</span> <span class="light">hangover symptons</span></p>
+					<p><span class="large">PROMOTES</span> <span class="light">long-term wellness</span></p>
+				</div>
+			</div>
+		</div>
+		<div class="wrapper wrapper-navbar-hp">
+			<div class="container">
+				<div class="row">
+					<div class="navbar-hp col-sm-2"><a href="#">Shop Online</a></div>
+					<div class="navbar-hp col-sm-2"><a href="previve-story.html">The Previve Story</a></div>
+					<div class="navbar-hp col-sm-2"><a href="how-it-works.html">How It Works</a></div>
+					<div class="navbar-hp col-sm-2"><a href="why-four-pills.html">Why Four Pills?</a></div>
+					<div class="navbar-hp col-sm-2"><a href="faq.html">FAQ</a></div>
+					<div class="navbar-hp col-sm-2"><a class="inactive-link" href="#">Press</a></div>
+				</div>
+			</div>
+		</div>
+		<div class="wrapper wrapper-disclaimer">
+			<div class="container">
+				<p class="disclaimer">
+					Remember, this product does not prevent intoxication, alcohol poisoning, or alcohol abuse. Please drink responsibly. Never drink and drive! Do not consume alcohol if you are not of the legal age to do so, while pregnant or 
+					nursing. These statements have not been evaluated by the Food and Drug Administration and Previve is not intended to diagnose, mitigate, treat, cure, or prevent any disease. Practice safe drinking!
+				</p>
+			</div>
+		</div>
+		<script src="assets/js/jquery.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+	</body>
 </html>
